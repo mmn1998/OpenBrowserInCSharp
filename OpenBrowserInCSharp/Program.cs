e50356fa07072dd6e65e46525727149efaa380cd
mmn1998 <mm.nomani.1998@gmail.com>
2023-08-30 09:02:47 +0330

    Add project files.

---
+++ OpenBrowserInCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+public class Program
+{
+
+public static void Main(string[] args)
+    {
+        Process.Start(new ProcessStartInfo("https://www.google.com/") { UseShellExecute = true });
+    }
+}
